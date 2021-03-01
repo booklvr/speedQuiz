06b9eef92ccc55c7a428fcea73d28b70fc5f79d3
Nicholas de Waal <nick.a.dewaal@gmail.com>
2021-03-01 15:41:25 +0900

    change port

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,14 @@ app.use('/api/wordList', wordListRoutes)
 // app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/public')))
+  // app.use(express.static(path.join(__dirname, '/frontend/public')))
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')))
 
-  app.get('* ', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'))
+  app.get(
+    '* ',
+    (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    // res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
@@ -43,7 +47,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3002
 
 app.listen(
   PORT,
